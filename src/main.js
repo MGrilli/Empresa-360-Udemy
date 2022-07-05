404c68a6e9e0d716768426937d0a2238759d0cbd
Marco Grilli <marco.grilli15@gmail.com>
2022-07-04 22:39:21 -0300

    Configurações inciais do Vue Router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
 
-createApp(App).mount('#app')
+
+const routes = [
+    {
+        path: '/home', //localhost:8080/home
+        component: Home
+    },
+    {
+        path: '/login', //localhost:8080/login
+        component: Login
+    }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+
+createApp(App).use(router).mount('#app')
