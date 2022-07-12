8cee9e71211a36a19b3ff09f2bc62f98db63c522
Marco Grilli <marco.grilli15@gmail.com>
2022-07-12 20:21:35 -0300

    Implementaçao navegaçao programatica

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,57 @@
 <template>
-
     <div>
-        <h3>Login</h3>
-        <router-link :to="{ path: '/home' }">Navegar para "/home"</router-link>
+        <nav class="navbar navbar-light bg-light menu-superior">
+            <div class="container">
+                <a class="navbar-brand" href="#">Empresa 360</a>
+                <div class="navbar-nav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/">Voltar</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="row mt-5">
+            <div class="col-6 offset-3">
+                <div class="card">
+                    <p class="card-header">Entrar</p>
+                    <div class="card-body">
+                        <div class="mb-3">
+                            <input type="email" class="form-control" placeholder="E-mail">
+                        </div>
+                        <div class="mb-3">
+                            <input type="password" class="form-control" placeholder="Senha">
+                        </div>
+                        <button class="btn btn-primary" @click="navegarPara">Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-   
 </template>
+
+<script>
+export default {
+    name: 'Login',
+    methods: {
+        navegarPara() {
+            //console.log(this.$router)
+
+            
+
+            this.$router.push('/home')
+
+            //this.$router.replace('/home')
+
+            //this.$router.forward() Avançar no histórico de rotas do navegador
+
+            //this.$router.back() retornar no histórico de rotas do navegador
+
+            //this.$router.go(2) avançar ou retornar no histórico de rotas do navegador
+        }
+    }
+    
+}
+</script>
