985ee81624b9556ac578344dee01764eccf81ece
Marco Grilli <marco.grilli15@gmail.com>
2022-07-22 21:30:27 -0300

    Atualizando router com exemplos de rotas aninhadas

--- src/router.js
+++ src/router.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 import Site from '@/views/Site.vue'
+import Vendas from '@/components/vendas/Vendas.vue'
+import Leads from '@/components/vendas/Leads.vue'
+import Contratos from '@/components/vendas/Contratos.vue'
+import Servicos from '@/components/servicos/Servicos.vue'
 
 const routes = [
     {
@@ -10,7 +14,27 @@ const routes = [
     },
     {
         path: '/home', //localhost:8080/home
-        component: Home
+        component: Home,
+        children: [
+            {
+                path: 'vendas', //localhost:8080/home/vendas
+                component: Vendas,
+                children: [
+                    {
+                        path: 'leads', //localhost:8080/home/home/vendas/leads
+                        component: Leads
+                    },
+                    {
+                        path: 'contratos', //localhost:8080/home/vendas/contratos
+                        component: Contratos
+                    }
+                ]
+            },
+            {
+                path: 'servicos', //localhost:808/home/servicos
+                component: Servicos
+            }
+        ]
     },
     {
         path: '/login', //localhost:8080/login
